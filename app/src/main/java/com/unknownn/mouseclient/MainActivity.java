ee0b961d45dfdb5dfaf1ed9eb3d0f308ad73fb6e
Abu Saeed <abusaeed2433@gmail.com>
2023-12-23 23:30:34 +0600

    socket transfer done. Works on emulator only

--- app/src/main/java/com/unknownn/mouseclient/MainActivity.java
+++ app/src/main/java/com/unknownn/mouseclient/MainActivity.java
@@ -3,12 +3,36 @@ package com.unknownn.mouseclient;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.unknownn.mouseclient.classes.WebSocketClient;
+import com.unknownn.mouseclient.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding = null;
+    private WebSocketClient socketClient = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        startWebsocketClient();
+
+        binding.tvHudai.setOnClickListener(view -> sendData(String.valueOf(System.currentTimeMillis())));
+    }
+
+    private void sendData(String message){
+        //if(socketClient == null) return;
+        System.out.println("Websocket button clicked");
+        socketClient.sendMessage(message);
+    }
+
+    private void startWebsocketClient(){
+        if(socketClient == null){
+            socketClient = new WebSocketClient();
+        }
     }
 }
--- app/src/main/java/com/unknownn/mouseclient/classes/WebSocketClient.java
+++ app/src/main/java/com/unknownn/mouseclient/classes/WebSocketClient.java
@@ -2,6 +2,13 @@ package com.unknownn.mouseclient.classes;
 
 import androidx.annotation.NonNull;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -11,64 +18,58 @@ import okio.ByteString;
 
 public class WebSocketClient {
     //private static final String WEBSOCKET_URL = "ws://27.147.190.170:8000";
-    private static final String WEBSOCKET_URL = "ws://192.168.29.150:8001";
+    //private static final String WEBSOCKET_URL = "ws://192.168.29.91:8000";
+    //private static final String WEBSOCKET_URL = "ws://localhost:8000";
+    private static final String WEBSOCKET_URL = "http://localhost:8000";
     private WebSocket webSocket = null;
+    private DataOutputStream outputStream = null;
+    ExecutorService service = Executors.newSingleThreadExecutor();
 
     public WebSocketClient() {
-        createClientObject();
+        createManualClient();
+        //createClientObject();
     }
 
-    public void createClientObject() {
-        OkHttpClient client = new OkHttpClient();
+    private void createManualClient(){
 
-        Request request = new Request.Builder()
-                .url(WEBSOCKET_URL)
-                .build();
+        ExecutorService service = Executors.newSingleThreadExecutor();
+        service.execute(() -> {
+            while (true) {
+                try {
+                    String host = "192.168.0.104";
+                    int port = 4275;
 
-        WebSocketListener webSocketListener = new WebSocketListener() {
-            @Override
-            public void onOpen(@NonNull WebSocket webSocket, @NonNull Response response) {
-                super.onOpen(webSocket, response);
-                System.out.println("WebSocket opened");
+                    System.out.println("Trying to connect to "+host+":"+port);
+
+                    Socket soc = new Socket(host, port);
+
+                    System.out.println("Websocket reading outputstream");
+                    outputStream = new DataOutputStream(soc.getOutputStream());
+                    System.out.println("Websocket ready to send data");
+                    break;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    try{
+                        Thread.sleep(3000);
+                    }catch (InterruptedException ignored){}
+                }
             }
-
-            @Override
-            public void onMessage(@NonNull WebSocket webSocket, @NonNull String text) {
-                super.onMessage(webSocket, text);
-                System.out.println("Received message: " + text);
-            }
-
-            @Override
-            public void onMessage(@NonNull WebSocket webSocket, @NonNull ByteString bytes) {
-                super.onMessage(webSocket, bytes);
-                System.out.println("Received bytes: " + bytes.hex());
-            }
-
-            @Override
-            public void onClosing(@NonNull WebSocket webSocket, int code, @NonNull String reason) {
-                super.onClosing(webSocket, code, reason);
-                System.out.println("WebSocket closing: " + code + " " + reason);
-                createClientObject();
-            }
-
-            @Override
-            public void onClosed(@NonNull WebSocket webSocket, int code, @NonNull String reason) {
-                super.onClosed(webSocket, code, reason);
-                System.out.println("WebSocket closed: " + code + " " + reason);
-            }
-
-            @Override
-            public void onFailure(@NonNull WebSocket webSocket, @NonNull Throwable t, Response response) {
-                super.onFailure(webSocket, t, response);
-                System.out.println("WebSocket failure: " + t.getMessage());
-            }
-        };
-
-        webSocket = client.newWebSocket(request, webSocketListener);
+        });
+        service.shutdown();
     }
 
     public void sendMessage(String message){
-        if(webSocket == null) return;
-        webSocket.send(message);
+        if(outputStream == null) return;
+
+        service.execute(() -> {
+            try {
+                System.out.println("Websocket trying to write");
+                outputStream.writeUTF(message);
+                System.out.println("Websocket Written");
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        });
     }
+
 }
